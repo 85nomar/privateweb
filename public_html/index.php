bda7b8102be3613f0e718ba7fb63d827ceb3e2d5
Raffael Wyss <raffael.wyss@gmail.com>
2013-03-22 05:40:12 +0100

    Grundstruktur Applikation + UnitTesting

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Das ist das Start-File für den public_html Ordner und hier wird die Haupt-
+ * Applikation gestartet
+ *
+ * @author      Raffael Wyss <raffael.wyss@gmail.com>
+ * @since       22.03.13
+ * @version     1.0.0
+ * @copyright   Copyright (c) 2013, PrivateWeb
+ * @package     PrivateWeb
+ */
---
+++ public_test/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Das ist das Start-File für den public_test Ordner und hier wird das
+ * Unit-Testing gemacht
+ *
+ * @author      Raffael Wyss <raffael.wyss@gmail.com>
+ * @since       22.03.13
+ * @version     1.0.0
+ * @copyright   Copyright (c) 2013, PrivateWeb
+ * @package     PrivateWeb_UnitTest
+ */
